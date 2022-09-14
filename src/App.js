f4897b21f93f62340d3834a5ac622acd31426d6c
J Wood <73401121+DFK333@users.noreply.github.com>
2022-09-13 19:48:09 -0500

    cl

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Daily Dynasties</h1>
+      <input type="number" min={5} max={10}></input>
+    </div>
+  );
+}
+
+export default App;
