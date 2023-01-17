b71134669435001aee3699121f882dd1493dbbb6
J Wood <73401121+DFK333@users.noreply.github.com>
2023-01-16 21:34:37 -0600

    added styled components to react frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,20 @@
 import { useState } from 'react';
-
+import styled from 'styled-components';
 import './App.css';
 
 import HomeSiteName from './components/Home/HomeSiteName';
-import Card from './components/Home/card';
-import CardItemHome from './components/Home/CardItemHome';
+import DFSCard from './components/Home/DFSCard';
+import DynastyCard from './components/Home/DynastyCard';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <HomeSiteName
-        dfsClass="daily homepage__daily active_homepage__daily "
-        dynastyClass="dynasty homepage__dynasties"
-      />
+      <HomeSiteName />
       <div className="card_container card_container--homepage">
-        <Card
-          whichCard="dfs"
-          class="card__item card__item--homepage card__item--dfshomepage"
-          message="DFS Tools & Projections"
-          items={[
-            <CardItemHome text="Projections By Position" />,
-            <CardItemHome text="Stacking Projections" />,
-            <CardItemHome text="Team & Game Environment Breakdowns" />,
-            <CardItemHome text="Ownership Projections" />,
-            <CardItemHome text="Lineup Builder" />,
-          ]}
-        />
-
-        <Card
-          whichCard="dynasty"
-          class="card__item card__item--homepage card__item--dynastyhomepage"
-          message="Dynasty Tools & Projections"
-          items={[
-            <CardItemHome text="Managed Lineup Projections" />,
-            <CardItemHome text="Sleeper Command Center" />,
-            <CardItemHome text="PCW Dynasty Trade Calculator" />,
-            <CardItemHome text="Player Exposure Tool" />,
-            <CardItemHome text="QB Prospect Model" />,
-          ]}
-        />
+        <DFSCard />
+        <DynastyCard />
       </div>
     </div>
   );
--- src/components/Home/HomeSiteName.jsx
+++ src/components/Home/HomeSiteName.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function HomeSiteName(props) {
   return (
-    <div className="sitename__container">
-      <h1 className="siteName">
-        <div className={props.dfsClass}>Daily</div>
-        <div className="homepage__spacer"></div>
-        <div className={props.dynastyClass}>Dynasties</div>
-      </h1>
-    </div>
+    <SiteNameContainer>
+      <SiteName>
+        <Daily>Daily</Daily>
+        <Spacer></Spacer>
+        <Dynasties>Dynasties</Dynasties>
+      </SiteName>
+    </SiteNameContainer>
   );
 }
+
+const SiteName = styled.h1`
+  /* display: flex;
+  justify-content: space-around; */
+  width: 80%;
+  margin-left: 12vw;
+  margin-right: auto;
+
+  /* display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(35rem, 1fr));
+  grid-gap: 2rem; */
+
+  display: flex;
+  justify-content: center;
+
+  letter-spacing: 0.3rem;
+`;
+
+const SiteNameContainer = styled.div`
+  font-size: 3rem;
+  margin-bottom: 3rem;
+  margin-top: 3rem;
+`;
+
+const Daily = styled.div`
+  margin-left: 6rem;
+  background: linear-gradient(to right bottom, #212f45, #3e1f47);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const Spacer = styled.div`
+  background-image: linear-gradient(to right bottom, #3e1f47, #065a60);
+  width: 8vw;
+  height: 2vh;
+  align-self: flex-end;
+
+  border-radius: 3rem;
+`;
+
+const Dynasties = styled.div`
+  background: linear-gradient(to right bottom, #065a60, #212f45);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
 
 export default HomeSiteName;
