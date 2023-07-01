7a05e1608138bfc9baf1462ee497308830ae84de
J Wood <73401121+DFK333@users.noreply.github.com>
2023-06-30 19:45:11 -0500

    finished sending dynastyrankingsdata to db

--- daily-dynasties-front/src/pages/DynastyRankings.js
+++ daily-dynasties-front/src/pages/DynastyRankings.js
@@ -2,8 +2,8 @@ import MainNav from '@/components/MainNav';
 import styles from '@/styles/dynastyRankings.module.css';
 import clientPromise from '@/lib/mongodb';
 
-export default function DynastyRankings(dynastyAndRedraftPlayerObjects) {
-  console.log(dynastyAndRedraftPlayerObjects);
+export default function DynastyRankings(dynastyRankingsData) {
+  console.log(dynastyRankingsData);
   return (
     <div>
       <MainNav />
@@ -17,16 +17,14 @@ export async function getStaticProps() {
     const client = await clientPromise;
     const db = client.db('dailydynasties');
 
-    const dynastyAndRedraftPlayerObjects = await db
-      .collection('dynastyAndRedraftPlayerObjects')
+    const dynastyRankingsData = await db
+      .collection('dynastyRankingsData')
       .find({})
       .toArray();
     // console.log(dynastyAndRedraftPlayerObjects);
     return {
       props: {
-        dynastyAndRedraftPlayerObjects: JSON.parse(
-          JSON.stringify(dynastyAndRedraftPlayerObjects)
-        ),
+        dynastyRankingsData: JSON.parse(JSON.stringify(dynastyRankingsData)),
       },
     };
   } catch (e) {
