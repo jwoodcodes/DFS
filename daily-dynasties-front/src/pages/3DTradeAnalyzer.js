6776192c621616290f1e70c3e7c78d79c4c44b22
J Wood <73401121+DFK333@users.noreply.github.com>
2023-07-07 21:12:51 -0500

    3/4 way through PNODV scaffolding

--- daily-dynasties-front/src/pages/3DTradeAnalyzer.js
+++ daily-dynasties-front/src/pages/3DTradeAnalyzer.js
@@ -1,11 +1,51 @@
 import MainNav from '@/components/MainNav';
 import styles from '@/styles/tradeAnalyzer.module.css';
+import clientPromise from '@/lib/mongodb';
 
-export default function ThreeDTradeAnalyzer() {
+export default function ThreeDTradeAnalyzer(dynastyRankingsData) {
+  let dataLevelOne = dynastyRankingsData.dynastyRankingsData;
+  // console.log(dataLevelOne);
+  let dataLevelTwo;
+  dataLevelOne.map(function (data) {
+    // console.log(data);
+    dataLevelTwo = data;
+  });
+  // console.log(dataLevelTwo);
+  // console.log(dataLevelTwo.tradeAnalyzerDataObjectsArray);
+  const dataArray = dataLevelTwo.tradeAnalyzerDataObjectsArray;
+  console.log(dataArray);
+
   return (
-    <>
+    <div>
       <MainNav />
       <div className={styles.pageTitle}>3D Trade Analyzer</div>
-    </>
+      {dataArray.map(function (player) {
+        return (
+          <li>
+            {player.name} {player.age}
+          </li>
+        );
+      })}
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db('dailydynasties');
+
+    const dynastyRankingsData = await db
+      .collection('tradeAnalyzerData')
+      .find({})
+      .toArray();
+    // console.log(dynastyRankingsData);
+    return {
+      props: {
+        dynastyRankingsData: JSON.parse(JSON.stringify(dynastyRankingsData)),
+      },
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
