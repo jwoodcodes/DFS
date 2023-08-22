ad184f08a8994090fd80fc8c2f2e9902f3891d00
J Wood <73401121+DFK333@users.noreply.github.com>
2023-08-22 08:50:01 -0500

    s

--- daily-dynasties-front/src/components/tradeAnalyzer/TeamTwo.js
+++ daily-dynasties-front/src/components/tradeAnalyzer/TeamTwo.js
@@ -70,14 +70,17 @@ export default function TeamTwo({
               <div className={styles.individualPlayerRow} key={player.name}>
                 <span>{player.name}:</span>
                 <span className={styles.PRPSpan}>
-                  PRP-Score:{''} {player.PRPScore}
+                  PRP Score:{''} {player.PRPScore}
                 </span>
                 <span className={styles.PNODVSpan}>
-                  PNODV-Score: {''}
+                  PNODV Score: {''}
                   {player.PNODVScore}{' '}
                 </span>
                 <span className={styles.RVSSpan}>
-                  RVS-Score: {''} {player.RVSScore}
+                  RVS Score: {''} {player.RVSScore}
+                </span>
+                <span className={styles.marketValueSpan}>
+                  Market Value: {player.marketValue}
                 </span>
               </div>
             );
