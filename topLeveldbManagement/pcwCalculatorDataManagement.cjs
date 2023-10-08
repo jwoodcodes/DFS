2b3fed24e9d6e0f3399e67d78539742c7f2f99b5
Jay Wood <jwoodcodes@gmail.com>
2023-10-08 14:37:26 -0500

    10/8 dynasty ranings update

--- topLeveldbManagement/pcwCalculatorDataManagement.cjs
+++ topLeveldbManagement/pcwCalculatorDataManagement.cjs
@@ -3880,40 +3880,40 @@ const testfunc = async function () {
 
   // ///////////uncomment below to push all trade analyzer data from above to db named tradeAnalyzerData
 
-  // const url =
-  //   'mongodb+srv://devJay:Hesstrucksarethebest@dailydynasties.syom4sb.mongodb.net/test';
-  // const client = new MongoClient(url);
+  const url =
+    'mongodb+srv://devJay:Hesstrucksarethebest@dailydynasties.syom4sb.mongodb.net/test';
+  const client = new MongoClient(url);
 
-  // // The database to use
-  // const dbName = 'dailydynasties';
+  // The database to use
+  const dbName = 'dailydynasties';
 
-  // async function runTradeAnalyzerData() {
-  //   try {
-  //     await client.connect();
-  //     console.log('Connected correctly to server');
-  //     const db = client.db(dbName);
+  async function runTradeAnalyzerData() {
+    try {
+      await client.connect();
+      console.log('Connected correctly to server');
+      const db = client.db(dbName);
 
-  //     const col = db.collection('tradeAnalyzerData');
+      const col = db.collection('tradeAnalyzerData');
 
-  //     // Construct a document
-  //     let tradeAnalyzerData = {
-  //       tradeAnalyzerDataObjectsArray,
-  //     };
+      // Construct a document
+      let tradeAnalyzerData = {
+        tradeAnalyzerDataObjectsArray,
+      };
 
-  //     // Insert a single document, wait for promise so we can read it back
-  //     const p = await col.insertOne(tradeAnalyzerData);
-  //     // Find one document
-  //     const myDoc = await col.findOne();
-  //     // Print to the console
-  //     // console.log(myDoc);
-  //   } catch (err) {
-  //     console.log(err.stack);
-  //   } finally {
-  //     await client.close();
-  //   }
-  // }
+      // Insert a single document, wait for promise so we can read it back
+      const p = await col.insertOne(tradeAnalyzerData);
+      // Find one document
+      const myDoc = await col.findOne();
+      // Print to the console
+      // console.log(myDoc);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
+    }
+  }
 
-  // runTradeAnalyzerData().catch(console.dir);
+  runTradeAnalyzerData().catch(console.dir);
 };
 // console.log(myJSON);
 testfunc();
