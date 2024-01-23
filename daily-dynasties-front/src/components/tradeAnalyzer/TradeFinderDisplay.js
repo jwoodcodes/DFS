87671ee0da15fbd199e7cba09438a9004fd076f5
J Wood <73401121+DFK333@users.noreply.github.com>
2024-01-22 18:42:18 -0600

    first trade finder deploy

--- daily-dynasties-front/src/components/tradeAnalyzer/TradeFinderDisplay.js
+++ daily-dynasties-front/src/components/tradeAnalyzer/TradeFinderDisplay.js
@@ -2,17 +2,25 @@ import React from 'react';
 import styles from '@/styles/tradeFinder.module.css';
 
 export default function TradeFinderDisplay({ completedTradesToPassToDisplay }) {
-  // console.log(completedTradesToPassToDisplay)
+  //   console.log(completedTradesToPassToDisplay);
 
-  completedTradesToPassToDisplay.map(tradeArray => {
-    // console.log(tradeArray)
-    // tradeArray.map(trade => {
-    //   // console.log(trade)
-    //   for (const [key, value] of Object.entries(trade)) {
-    //     // console.log(`${key}: ${value}`);
-    //   }
-    // });
-  });
+  return (
+    <div>
+      {completedTradesToPassToDisplay.map(tradeArray => {
+        // console.log(tradeArray);
+        return tradeArray.map(trade => {
+          //   console.log(trade);
 
-  return <div></div>;
+          return (
+            <div>
+              <div key={trade.transactionTime}>
+                {trade.leagueName} {trade.dateOfTrade}
+              </div>
+              <div>{trade.playersInvovled}</div>
+            </div>
+          );
+        });
+      })}
+    </div>
+  );
 }
