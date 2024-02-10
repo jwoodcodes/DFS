0f0541324ad1001964dd46811898a3ca7c82defd
J Wood <73401121+DFK333@users.noreply.github.com>
2024-02-10 11:14:19 -0600

    RV article One data analysis

--- historic weekly CSV's/dataAnalysisForRotovizSeriesOne.js
+++ historic weekly CSV's/dataAnalysisForRotovizSeriesOne.js
@@ -3,6 +3,8 @@ const { MongoClient } = require('mongodb');
 const before2023SeasonRVRedraftRankingsTEP = require('./before2023SeasonRVRedraftRankings');
 const before2023SeasonFFPCRedraftADP = require('./FFPCBefore2023RedraftADP');
 
+const before2023SeasonRVDynastyRankings = require('./week-6-2023/rvDynastyRankingsTEP');
+
 const alltradeCalculaterDataArray = [];
 const newData = [];
 
@@ -116,19 +118,195 @@ const testfunc = async function () {
   // below here is where the comparing august 2023 dynasty to value to current dynasty value is being done
   //
   // need to sanitize the name inputs from both before comparing names!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+  let num = 0;
   playerArray.forEach(player => {
     // console.log(player.player.name, player.value);
 
+    let sanitizedPlayerName = player.player.name;
+
+    // sanitizing playerArray names
+
+    sanitizedPlayerName = sanitizedPlayerName
+      .replace("'", '')
+      .replace('.', '')
+      .replace('.', '');
+    if (sanitizedPlayerName.includes('Jr')) {
+      // console.log(playerObject['"PLAYER NAME"']);
+      sanitizedPlayerName = sanitizedPlayerName.replace('Jr', '');
+    }
+
     august2023PlayerArray.forEach(augPlayer => {
-      if (player.player.name === augPlayer.name) {
-        console.log(augPlayer.name);
+      // sanitizing augplayer names
+
+      let sanitizedAugPlayerName = augPlayer.name;
+
+      sanitizedAugPlayerName = sanitizedAugPlayerName
+        .replace("'", '')
+        .replace('.', '')
+        .replace('.', '');
+      if (sanitizedAugPlayerName.includes('Jr')) {
+        // console.log(playerObject['"PLAYER NAME"']);
+        sanitizedAugPlayerName = sanitizedAugPlayerName.replace('Jr', '');
+      }
+
+      if (sanitizedPlayerName === sanitizedAugPlayerName) {
+        //////////////////////////////////
+        //////////////////////////////////////
+        /////////////////////////////////////////
+        ////////////////////////////////////////
+        //               do everything in here
+        ///////////////////////////////
+        //////////////////////////////////
+
+        // console.log(player);
+        // console.log(augPlayer);
+
+        // console.log(sanitizedAugPlayerName);
+
+        // console.log(sanitizedAugPlayerName);
+        // console.log(augPlayer.marketValue);
+        // console.log(player.player.maybeAge);
+
+        let valueDifferenceFromAugustToThisOffseason =
+          player.value - augPlayer.marketValue;
+
+        let valueLostWillBeAPostiveNumberValueDiff =
+          augPlayer.marketValue - player.value;
+
+        // console.log(
+        //   sanitizedAugPlayerName,
+        //   valueDifferenceFromAugustToThisOffseason
+        // );
+
+        // console.log(augPlayer);
+
+        let augAge = player.player.maybeAge - 0.8;
+        // && augAge < 25
+
+        // for value risers
+
+        // if (valueDifferenceFromAugustToThisOffseason > 800) {
+        //   console.log(
+        //     sanitizedAugPlayerName,
+        //     valueDifferenceFromAugustToThisOffseason,
+        //     player.player.maybeAge
+        //   );
+        // }
+
+        // for value droppers
+
+        // valueLostWillBeAPostiveNumberValueDiff > 800 &&
+
+        if (
+          augAge > 23 &&
+          augPlayer.marketValue > 800 &&
+          valueLostWillBeAPostiveNumberValueDiff > 800
+        ) {
+          // num = num + 1;
+          // console.log(
+          //   num,
+          //   sanitizedAugPlayerName,
+          //   valueDifferenceFromAugustToThisOffseason,
+          //   player.player.maybeAge
+          // );
+        }
+
+        before2023SeasonRVDynastyRankings.forEach(augRVDynastyPlayer => {
+          // console.log(augRVDynastyPlayer['"Player"'].slice(1, -1));
+
+          let sanitizedRVPlayerName = augRVDynastyPlayer['"Player"'].slice(
+            1,
+            -1
+          );
+
+          sanitizedAugPlayerName = sanitizedAugPlayerName
+            .replace("'", '')
+            .replace('.', '')
+            .replace('.', '');
+          if (sanitizedAugPlayerName.includes('Jr')) {
+            // console.log(playerObject['"PLAYER NAME"']);
+            sanitizedAugPlayerName = sanitizedAugPlayerName.replace('Jr', '');
+          }
+
+          let whoRVwasLowerOn =
+            +augRVDynastyPlayer['"Rank"'] - augPlayer.marketOverallRank;
+          let whoRVwasHigherOn =
+            augPlayer.marketOverallRank - +augRVDynastyPlayer['"Rank"'];
+
+          // +augRVDynastyPlayer['"Rank"'] < 50
+          // augAge > 25
+          // valueDifferenceFromAugustToThisOffseason > 0
+          // augPlayer.marketValue > 800 &&
+
+          if (sanitizedAugPlayerName === sanitizedRVPlayerName) {
+            if (
+              whoRVwasLowerOn >= 5 &&
+              +augRVDynastyPlayer['"Rank"'] < 100 &&
+              augAge > 25
+            ) {
+              num = num + 1;
+              console.log(
+                num,
+                sanitizedRVPlayerName,
+                // +augRVDynastyPlayer['"Rank"'],
+                valueDifferenceFromAugustToThisOffseason,
+                player.player.maybeAge
+              );
+            }
+          }
+        });
       }
     });
   });
 
   //   console.log(august2023PlayerArray);
 };
+
+/// notes
+
+// players who gained at least 800 value:
+
+// 49 total
+// 78% (38) below 25
+// 41% (20) entered season under 23
+
+// players who lost at least 800 value:
+
+// 48 total
+// 38% (18) under 25
+// 8% (4) under 23
+
+// 168 players entered last season OVER the age of 23 with a market value of at least 800:
+// 16.7% (28) gained at least 800 in value
+// 26.1% (44) lost at least 800 in value
+
+//101 players entered last season OVER 25 with a market value of at least 800
+// 10.8% (11) gained at least 800 in value
+// 29.7% (30) lost at least 800 in value
+
+// 46 players entered last season UNDER 23 with a market value of at least 800:
+// 43.4% (20) gained at least 800 value
+// only 8.7% (4 total) lost at least 800 value
+
+// players ranked in top 50 or RV dynasty rankings that RV was at least 2 spots higher on than market:
+// 19 total
+// 79% (15) went up in value regardless of age
+// only 5% (1) went down by more than 250 value points
+// 12 entered season under 25, 75% (9) went up in value, none went down by more than 250 value points
+// of players that entered season over 25: only 1/7 went down at all, tony pollard by 1075
+
+//
+// players who enetered season in top 100 of RV dynasty rankings && RV was at least 5 spots HIGHER on
+// 47 total
+// 64% (30) at least maintained value, 16/26 below 25, 8/13 below 23
+// 51% (24) gained in value by more than 250 points
+// 19.1% (9) went down by more than 250 value points, 6/26 below 25, 3/13 below 23
+
+// players who enetered season in top 100 of RV dynasty rankings && RV was at least 5 spots LOWER on
+// 19 total
+// 15.7% (3 total) at least maintained value
+// 15.7% (3 total) gained in value by more than 250 points, all 3 were over over 25
+// 79% (15) went down by more than 250 value points, 73% (11/15) over 25,
 
 testfunc();
 
