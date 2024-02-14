1dd52af85e440be06c8102d203b2ca4f28887ee4
Jay Wood <jwoodcodes@gmail.com>
2024-02-13 20:25:42 -0600

    unWR model work

--- historic weekly CSV's/dataAnalysisForRotovizSeriesOne.js
+++ historic weekly CSV's/dataAnalysisForRotovizSeriesOne.js
@@ -393,7 +393,7 @@ const testfunc = async function () {
 
 /////////////////////////////////////////// bringing in RV dynasty rankings and RV dynasty rankings vs. market
 
-// of the 11 players who were over 25 and gained more than 800 value points RV dynasty rankings were at least 6 higher on 8/11 and at least 2 higher on 9/11
+
 
 // players ranked in top 50 or RV dynasty rankings that RV was at least 2 spots higher on than market:
 // 19 total
@@ -415,6 +415,8 @@ const testfunc = async function () {
 // 15.7% (3 total) gained in value by more than 250 points, all 3 were over over 25
 // 79% (15) went down by more than 250 value points, 73% (11/15) over 25,
 
+// of the 11 players who were over 25 and gained more than 800 value points RV dynasty rankings were at least 6 higher on 8/11 and at least 2 higher on 9/11
+
 ////////////
 ///////////
 
@@ -424,7 +426,7 @@ const testfunc = async function () {
 
 // for this an overperfrom is finishing 5 spots better if were top 12 or 10 spots better if outside top 12 august positioal ADP
 
-// below 23
+// below 23 years old
 
 //// QB- 4 total
 // 2 overperform- 1/2 gained, 1/2 lost (howell), 1/2 gained > 250, 1/2 lost > 250, 1/2 gained greater than 800, 0/2 lost more than 800
@@ -442,7 +444,7 @@ const testfunc = async function () {
 // 1 overperform- 1/1 gained, 0/1 lost, 1/1 gained > 250, 0/1 lost > 250, 1/1 gained greater than 800 , 0/1 lost more than 800
 // 2 underperform- 1/2 gained, 1/2 lost, 0/2 gained > 250, 0/2 lost > 250, 0/2 gained > 800, 0/2 lost > 800
 
-// between 23.0 and 25.0
+// between 23.0 and 25.0 years old
 
 // QB- 9 total
 // 3 overperform- 2/3 gained, 1/3 lost (ridder), 2/3 gained > 250, 1/3 lost > 250, 2/3 gained greater than 800, 1/3 lost more than 800 (ridder)
@@ -460,7 +462,7 @@ const testfunc = async function () {
 // 4 overperform- 4/4 gained, 0/4 lost, 4/4 gained > 250, 0/4 lost > 250, 2/4 gained greater than 800, 0/4 lost more than 800
 // 2 underperform- 0/2 gained, 2/2 lost, 0/2 gained > 250, 1/2 lost > 250, 0/2 gained > 800, 0/2 lost > 800
 
-// over 25.0
+// over 25.0 years old
 // QB- 19 total
 //// 4 overperform- 4/4 gained, 0/4 lost, 4/4 gained > 250, 0/4 lost > 250, 1/4 gained greater than 800, 0/4 lost more than 800 (ridder)
 // 7 underperformed- 0/7 gained, 7/7 lost, 0/7 gained > 250, 6/7 lost > 250, 0/7 gained > 800, 5/7 lost > 800
@@ -479,17 +481,20 @@ const testfunc = async function () {
 
 // totals from above:
 
-// under 23- 38 total who started year > 800 market value
+// under 23- 
+// 38 total who started year > 800 market value
 
 // 13 (34%) overperform- 11/13(85%) gained, 2/13(15%) lost, 10/13(77%) gained > 250, 1/13(8%) lost > 250, 9/13(69%) gained greater than 800, 0/13(0%) lost more than 800
 // 17 (45%) underperform- 8/17(47%) gained, 9/17(53%) lost, 3/17(18%) gained > 250, 6/17(35%) lost > 250, 1/17(5%) gained > 800, 2/17(29%) lost > 800
 
-// between 23 and 25 - 64 total who started year > 800 market value
+// between 23 and 25 - 
+// 64 total who started year > 800 market value
 
 // 21/64(32%) overperform- 17/21(84%) gained, 4/17(23%) lost, 17/21(84%) gained > 250, 2/21(10%) lost > 250, 9/21(42%) gained greater than 800, 1/21(5%) lost more than 800
 // 27/64(42%) underperform- 1/27(3%) gained, 26/27(96%) lost, 1/27(3%) gained > 250, 24/27(92%) lost > 250, 1/27(4%) gained > 800, 12/27(38%) lost > 800
 
-// over 25.0 - 96 total who started year > 800 market value
+// over 25.0 - 
+// 96 total who started year > 800 market value
 
 // 20/96(21%) overperform- 14/20(70%) gained, 6/20(30%) lost, 11/20(55%) gained > 250, 5/20(25%) lost > 250, 4/20(20%) gained greater than 800, 1/20(5%) lost more than 800
 // 40/96(42%) underperform- 1/40(2%) gained, 39/40(98%) lost, 1/40(2%) gained > 250, 36/40(95%) lost > 250, 0/40(0%) gained > 800, 18/40(45%) lost > 800
