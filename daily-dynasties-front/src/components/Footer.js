2497462a34da26509fc3ad4a437a14f4726bba5b
J Wood <73401121+DFK333@users.noreply.github.com>
2024-08-15 20:54:31 -0500

    change footer back

--- daily-dynasties-front/src/components/Footer.js
+++ daily-dynasties-front/src/components/Footer.js
@@ -2,9 +2,9 @@ import styles from '@/styles/footer.module.css';
 import Link from 'next/link';
 
 export default function Footer() {
-    return (
-        <div className={styles.footerWrapper}>
-           <Link href="https://twitter.com/FFcoder" className={styles.footerLink}>@FFCoder</Link> 
-        </div>
-    )
+  return (
+    <div className={styles.footerWrapper}>
+      {/* <Link href="https://twitter.com/FFcoder" className={styles.footerLink}>@FFCoder</Link>  */}
+    </div>
+  );
 }
